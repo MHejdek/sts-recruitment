4749ee20e45081f6ec10724765cd78c85797c284
Maciej Hejduk <maciej.hejduk@linkfactory.dk>
2022-04-01 17:27:11 +0200

    STS-4: Add ORM mapping to Wallet entity and create migration file

--- src/Entity/Wallet.php
+++ src/Entity/Wallet.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Repository\WalletRepository;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+#[Entity(repositoryClass: WalletRepository::class)]
 class Wallet
 {
+    #[Id, GeneratedValue, Column(type: 'integer')]
     private int $id;
 
-    private float $amount;
+    #[Column(type: 'integer')]
+    private int $amount;
 
-    public function __construct(
-        string $name,
-        float $amount
-    )
+    public function __construct(int $amount)
     {
         $this->amount = $amount;
     }
